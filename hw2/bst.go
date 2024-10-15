828f60101adc49185fb934be4349c2720dbd6a89
Niko Finlertet <nikofinlertet@gmail.com>
2024-10-16 00:05:13 +0300

    Work complete

--- hw2/bst.go
+++ hw2/bst.go
@@ -2,25 +2,184 @@ package main
 
 import "fmt"
 
-type node struct {
+type Node struct {
 	value int
-	left  *node
-	right *node
+	left  *Node
+	right *Node
+}
+type Tree struct{ root *Node }
+
+// add вставляет новый узел в бинарное дерево поиска
+func (t *Tree) add(num int) {
+	p := t.root
+
+	//!!! t.root - error!!!
+	if t.root == nil {
+		t.root = &Node{value: num}
+		return
+	}
+
+	for { //Идем циклом
+		if num <= p.value { //Если меньше, то идем слева
+			if p.left == nil {
+				p.left = &Node{value: num}
+				return
+			}
+			p = p.left
+		} else {
+			if p.right == nil {
+				p.right = &Node{value: num}
+				return
+			}
+			p = p.right
+		}
+	}
 }
 
-func add(num int) {
+func (t *Tree) deleteTree() {
+	t.root = nil
+}
+
+func (t *Tree) delete(num int) {
+	p := t.root
+	pred := p
+
+	if t.root == nil {
+		return
+	}
+
+	for {
+		//Find
+		if num < p.value {
+			if p.left != nil {
+				pred = p
+				p = p.left
+			}
+		} else if num > p.value {
+			if p.right != nil {
+				pred = p
+				p = p.right
+			}
+		} else {
+			//If find --> DELETE
+			if p.left == nil && p.right == nil {
+				if pred.left == p {
+					pred.left = nil
+				} else {
+					pred.right = nil
+				}
+				return
+
+			} else if p.left == nil {
+				if pred.left == p {
+					pred.left = p.right
+				} else {
+					pred.right = p.right
+				}
+
+			} else if p.right == nil {
+				if pred.left == p {
+					pred.left = p.left
+				} else {
+					pred.right = p.left
+				}
+
+			} else {
+				min := minimumNode(p.right, pred)
+				p.value = min.value
+				return
+			}
+		}
+	}
+}
+
+func minimumNode(this *Node, last *Node) *Node {
+	p := this
+	pred := last
+
+	for p.left != nil {
+		pred = p
+		p = p.left
+	}
+
+	if pred.left == p {
+		pred.left = nil
+	}
+
+	return p
+}
+
+func (t *Tree) isExist(num int) bool {
+	p := t.root
+
+	if t.root == nil {
+		return false
+	}
+
+	for {
+		if num == p.value {
+			return true
+		}
+
+		if num < p.value {
+			if p.left != nil {
+				p = p.left
+			}
+		} else if num < p.value {
+			if p.right != nil {
+				p = p.right
+			}
+		} else {
+			return false
+		}
+	}
+
+}
+
+func (t *Tree) print() {
+	if t.root != nil {
+		t.root.print()
+	} else {
+		fmt.Println("Tree not exist")
+	}
+	fmt.Println("")
+}
+
+func (n *Node) print() {
+	if n != nil {
+		n.left.print()
+		fmt.Print(n.value, " ")
+		n.right.print()
+	}
 }
 
 func main() {
-	n := node{value: 1}
-	n.left = &node{value: 2}
-	n.right = &node{value: 3}
-	n.left.left = &node{value: 4}
-	n.left.right = &node{value: 5}
-	n.right.left = &node{value: 6}
-	n.right.right = &node{value: 7}
+	A := Tree{}
+	B := Tree{}
 
-	fmt.Println(n.value)
+	A.add(50)
+	A.add(30)
+	A.add(20)
+	A.add(40)
+	A.add(70)
+	A.add(60)
 
-	fmt.Println(n.left.value)
+	B.add(1)
+	B.add(599)
+	B.add(199)
+	B.add(2545)
+
+	fmt.Println("A:")
+	A.print()
+	fmt.Println("B:")
+	B.print()
+
+	fmt.Println("exist '50' is", A.isExist(50))
+	A.delete(50)
+	fmt.Println("exist '50' is", A.isExist(50))
+	A.print()
+
+	B.deleteTree()
+	B.print()
+
 }
